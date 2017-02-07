7a37fae564ae4bc297a0bd5d768c259aed5ab032
Olufemi Oye <olufemi.oye@gmail.com>
2017-02-07 22:20:03 +0100

    was he demo

---
+++ demo/index.php
@@ -0,0 +1,334 @@
+<!doctype html>
+<html>
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width-device-width", initial-scale="1.0">
+      <title>Washe || Homepage</title>
+      <script type="text/javascript" src="js/jquery-3.1.1.min-1"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/styles.css" media="(min-width:992px)">
+</head>
+<body>
+	 <!-- navigational bar begins
+    ================================================== -->
+	<div class="container-fluid">
+	      <div class="row navbar-default">
+	      	  <div class="welog">
+	  	      <div class="welcome navbar-left">Welcome, Guest!</div>
+	  	      <div class="logsign navbar-right"><a href="#">Login</a> or <a href="#">Create An Account</a></div>
+	  	      </div>
+          </div>
+	  <a href="index.php" class="navbar-brand navbar-left">Washe logo</a>
+      <ul class="nav nav-pills navbar-right" role="tablist">
+      	<li><a href="#">HOME</a></li>
+      	<li><a href="#">ABOUT</a></li>
+      	<li><a href="#">SERVICES</a></li>
+      	<li><a href="#">HOW IT WORKS</a></li>
+      	<li><a href="#">SUPPORT</a></li>
+      </ul>
+    </div><!-- /.navigational bar ends -->
+
+ <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    	 <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="img/caro2.jpg">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>WASHE</h1>
+              <p class="lead">An online laundry marketplace that connects you to the best laundry service</p>
+              <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/caro1.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <a class="btn btn-large btn-primary" href="#">Learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/caro3.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <a class="btn btn-large btn-primary" href="#">Browse gallery</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+    </div><!-- /.carousel -->
+
+    	 <!-- wash partner recommend
+    ================================================== -->
+ <div class="container-fluid">
+    <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-3">
+          	  <div class="wash">
+          	     <img src="img/wash.jpg" width="35%" height="30%">
+          	     <h2>WASH</h2>
+          	     <p>Just was without having to create an account.</p><br>
+          	     <a class="btn btn-large btn-primary" href="#">Click here</a>
+          	  </div>
+          </div>
+          <div class="col-md-3">
+          	   <div class="part">
+          	   	 <img src="img/partner.jpg" width="35%" height="30%">
+          	   	 <h2>PARTNER</h2>
+                 <p>Register to buy bulk wash on credit and resell it on our platform.</p>
+                 <a class="btn btn-large btn-primary" href="#">Sign up</a>
+          	   </div>
+          </div>
+          <div class="col-md-3">
+          	    <div class="recom">
+          	      <img src="img/recommend.jpg" width="35%" height="30%">
+          	      <h2>RECOMMEND</h2>
+          	      <p>Recommend or register people and you get paid.</p><br>
+                  <a class="btn btn-large btn-primary" href="#">Get started</a>
+          	    </div>
+          </div>
+          <div class="col-md-1"></div>
+    </div>
+ </div><br><br><br><!-- /.wash partner recommend -->
+ <!-- Subscribe
+    ================================================== -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12"><img src="img/subimage.jpg" width="100%" height="300px"></div>
+	</div>
+	<div class="subscribe">
+	     <div class="row">
+             <div class="col-md-3"></div>
+             <div class="col-md-6"><h2>Subscribe To Our Newsletters</h2></div>
+             <div class="col-md-3"></div>
+	     </div>
+	     <div class="row">
+             <div class="col-md-2"></div>
+             <div class="col-md-8"><p>Subscribe to our newsletters to be updated with latest updates, products, bonuses and offers</h2></div>
+             <div class="col-md-2"></div>
+	     </div>
+	     <div class="row">
+	     	<div class="col-md-2"></div>
+	     	<div class="col-md-10">
+	          <form class="form-inline">
+                   <div class="form-group form-group-lg">
+                         <input class="form-control" type="text" id="formGroupInputLarge" placeholder="Enter Your Email"> <button type="submit" class="btn btn-primary btn-lg">Subscribe</button>
+                   </div>
+
+              </form>
+            </div>
+	     </div>
+	</div>
+</div>
+ <!--end Subscribe
+    ================================================== -->
+
+
+  <!--footer
+    ================================================== --> 
+<footer>
+       <div class="container-fluid">
+              <div class="row">
+              	 <div class="foot">
+              	    <div class="col-md-2"></div>
+              	    <div class="col-md-3">
+                             <h2>Washe</h2>
+                             <p><a href="#">About Us</a></p>
+                             <p><a href="#">FAQs</a></p>
+                             <p><a href="#">Contact Us</a></p>
+                             <p><a href="#">Terms and Conditions</a></p>
+              	    </div>
+              	    <div class="col-md-3">
+                             <h2>Services</h2>
+                             <p><a href="#">Wash</a></p>
+                             <p><a href="#">Business Partner</a></p>
+                             <p><a href="#">Affiliate</a></p>
+                             <p><a href="#">Privacy Policy</a></p>
+              	    </div>
+              	    <div class="col-md-3">
+                             <h2>Get In Touch</h2>
+                             <p><a>Phone:1234567890</a></p>
+                             <p><a>Email:customercare@washe.com </a></p>
+                    </div>
+              	    <div class="col-md-1"></div>
+              	 </div>
+              </div>
+       </div>
+
+
+<!-- -Login Modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+    	<div class="modal-content login-modal">
+      		<div class="modal-header login-modal-header">
+        		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        		<h4 class="modal-title text-center" id="loginModalLabel">USER AUTHENTICATION</h4>
+      		</div>
+      		<div class="modal-body">
+      			<div class="text-center">
+	      			<div role="tabpanel" class="login-tab">
+					  	<!-- Nav tabs -->
+					  	<ul class="nav nav-tabs" role="tablist">
+					    	<li role="presentation" class="active"><a id="signin-taba" href="#home" aria-controls="home" role="tab" data-toggle="tab">Sign In</a></li>
+					    	<li role="presentation"><a id="signup-taba" href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Sign Up</a></li>
+					    	<li role="presentation"><a id="forgetpass-taba" href="#forget_password" aria-controls="forget_password" role="tab" data-toggle="tab">Forget Password</a></li>
+					  	</ul>
+					
+					  	<!-- Tab panes -->
+					 	<div class="tab-content">
+					    	<div role="tabpanel" class="tab-pane active text-center" id="home">
+					    		&nbsp;&nbsp;
+					    		<span id="login_fail" class="response_error" style="display: none;">Loggin failed, please try again.</span>
+					    		<div class="clearfix"></div>
+					    		<form>
+									<div class="form-group">
+								    	<div class="input-group">
+								      		<div class="input-group-addon"><i class="fa fa-user"></i></div>
+								      		<input type="text" class="form-control" id="login_username" placeholder="Username">
+								    	</div>
+								    	<span class="help-block has-error" id="email-error"></span>
+								  	</div>
+								  	<div class="form-group">
+								    	<div class="input-group">
+								      		<div class="input-group-addon"><i class="fa fa-lock"></i></div>
+								      		<input type="password" class="form-control" id="password" placeholder="Password">
+								    	</div>
+								    	<span class="help-block has-error" id="password-error"></span>
+								  	</div>
+						  			<button type="button" id="login_btn" class="btn btn-block bt-login" data-loading-text="Signing In....">Login</button>
+						  			<div class="clearfix"></div>
+						  			<div class="login-modal-footer">
+						  				<div class="row">
+											<div class="col-xs-8 col-sm-8 col-md-8">
+												<i class="fa fa-lock"></i>
+												<a href="javascript:;" class="forgetpass-tab"> Forgot password? </a>
+											
+											</div>
+											
+											<div class="col-xs-4 col-sm-4 col-md-4">
+												<i class="fa fa-check"></i>
+												<a href="javascript:;" class="signup-tab"> Sign Up </a>
+											</div>
+										</div>
+						  			</div>
+								</form>
+					    	</div>
+					    	<div role="tabpanel" class="tab-pane" id="profile">
+					    	    &nbsp;&nbsp;
+					    	    <span id="registration_fail" class="response_error" style="display: none;">Registration failed, please try again.</span>
+					    		<div class="clearfix"></div>
+					    		<form>
+									<div class="form-group">
+								    	<div class="input-group">
+								      		<div class="input-group-addon"><i class="fa fa-user"></i></div>
+								      		<input type="text" class="form-control" id="username" placeholder="Username">
+								    	</div>
+								    	<span class="help-block has-error" data-error='0' id="username-error"></span>
+								  	</div>
+								  	<div class="form-group">
+								    	<div class="input-group">
+								      		<div class="input-group-addon"><i class="fa fa-at"></i></div>
+								      		<input type="text" class="form-control" id="remail" placeholder="Email">
+								    	</div>
+								    	<span class="help-block has-error" data-error='0' id="remail-error"></span>
+								  	</div>
+						  			<button type="button" id="register_btn" class="btn btn-block bt-login" data-loading-text="Registering....">Register</button>
+									<div class="clearfix"></div>
+									<div class="login-modal-footer">
+						  				<div class="row">
+											<div class="col-xs-8 col-sm-8 col-md-8">
+												<i class="fa fa-lock"></i>
+												<a href="javascript:;" class="forgetpass-tab"> Forgot password? </a>
+											
+											</div>
+											
+											<div class="col-xs-4 col-sm-4 col-md-4">
+												<i class="fa fa-check"></i>
+												<a href="javascript:;" class="signin-tab"> Sign In </a>
+											</div>
+										</div>
+						  			</div>
+								</form>
+					    	</div>
+					    	<div role="tabpanel" class="tab-pane text-center" id="forget_password">
+					    		&nbsp;&nbsp;
+					    	    <span id="reset_fail" class="response_error" style="display: none;"></span>
+						    		<div class="clearfix"></div>
+						    		<form>
+										<div class="form-group">
+									    	<div class="input-group">
+									      		<div class="input-group-addon"><i class="fa fa-user"></i></div>
+									      		<input type="text" class="form-control" id="femail" placeholder="Email">
+									    	</div>
+									    	<span class="help-block has-error" data-error='0' id="femail-error"></span>
+									  	</div>
+									  	
+							  			<button type="button" id="reset_btn" class="btn btn-block bt-login" data-loading-text="Please wait....">Forget Password</button>
+										<div class="clearfix"></div>
+										<div class="login-modal-footer">
+							  				<div class="row">
+												<div class="col-xs-6 col-sm-6 col-md-6">
+													<i class="fa fa-lock"></i>
+													<a href="javascript:;" class="signin-tab"> Sign In </a>
+												
+												</div>
+												
+												<div class="col-xs-6 col-sm-6 col-md-6">
+													<i class="fa fa-check"></i>
+													<a href="javascript:;" class="signup-tab"> Sign Up </a>
+												</div>
+											</div>
+							  			</div>
+									</form>
+						    	</div>
+						  	</div>
+						</div>
+	      				
+	      			</div>
+	      		</div>
+	      		
+	    	</div>
+	   </div>
+ 	</div>
+ 	<!-- - Login Model Ends Here -->
+
+ <script>
+$(document).ready(function(){
+	$(document).on('click','.signup-tab',function(e){
+		e.preventDefault();
+	    $('#signup-taba').tab('show');
+	});	
+	
+	$(document).on('click','.signin-tab',function(e){
+	   	e.preventDefault();
+	    $('#signin-taba').tab('show');
+	});
+	    	
+	$(document).on('click','.forgetpass-tab',function(e){
+	 	e.preventDefault();
+	   	$('#forgetpass-taba').tab('show');
+	});
+});	
+</script>
+</footer>
+</body>
+</html>
